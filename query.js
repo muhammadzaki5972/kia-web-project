ba0e0cb98ecb23d533f309a4966f7f9e673e1c83
muhammadzaki5972 <muhammad.zacky27@gmail.com>
2026-07-01 09:40:52 +0700

    chore: clean up unused testing scripts

--- query.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { db } = require('./api/db.js');
-async function query() {
-  const result = await db.execute('SELECT * FROM data_perkara LIMIT 1');
-  console.log(result.rows);
-}
-query();
--- query_detail.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { db } = require('./api/db.js');
-async function query() {
-  const result = await db.execute("SELECT * FROM detail_perkara WHERE id = '001/I/KIA/2026'");
-  console.log(result.rows);
-}
-query();
